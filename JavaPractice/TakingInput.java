5fb6b40e6443f97ca663fa1004e6c99dd9a43385
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-04-17 22:57:49 +0530

    feat: add TakingInput class for user input

---
+++ JavaPractice/TakingInput.java
@@ -0,0 +1,13 @@
+package JavaPractice;
+import java.util.Scanner;
+
+public class TakingInput {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    System.out.println("Hello " + sc.nextLine());
+  }}
+  
+  
+
+ 
