8edc23522f4b836db62a23eecb68eac5d1ba86a1
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-04-17 23:01:41 +0530

    docs: explain how TakingInput uses Scanner for console input

--- JavaPractice/TakingInput.java
+++ JavaPractice/TakingInput.java
@@ -8,6 +8,7 @@ public class TakingInput {
     System.out.println("Hello " + sc.nextLine());
   }}
   
+  //here we learned about taking input from using Scanner class from the package java.util.Scanner. We created an object(using 'new') of the Scanner class and used the nextLine()(for printing strings) method to read a line of text from the user(System.in (input from the keyboard)). We then printed a greeting message that includes the our name.
   
 
  
