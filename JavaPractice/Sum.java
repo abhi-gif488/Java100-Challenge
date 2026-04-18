f7de5272d6b9e56b15c1a159a7efc464c2d3aed4
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-04-18 12:53:20 +0530

    test: change datatype to float for input and the output aswell

--- JavaPractice/Sum.java
+++ JavaPractice/Sum.java
@@ -3,12 +3,19 @@ import java.util.Scanner;
 public class Sum {
   public static void main(String[] args){
     Scanner sc = new Scanner (System.in);
-    System.out.println("Enter your first number: ");
-    int num1= sc.nextInt();
-      System.out.println("Enter your second number: ");
-      int num2 = sc.nextInt();
-      int sum = num1 + num2;
-      System.out.println("The Sum of this two numbers is : " + sum);
+    // System.out.println("Enter your first number: ");
+    // int num1= sc.nextInt();
+    //   System.out.println("Enter your second number: ");
+    //   int num2 = sc.nextInt();
+    //   int sum = num1 + num2;
+    //   System.out.println("The Sum of this two numbers is : " + sum);
   
+     System.out.println("Enter your first number: ");
+     float num1= sc.nextInt();
+     System.out.println("Enter your second number: ");
+     float num2 = sc.nextInt();
+     float sum = num1 + num2;
+     System.out.println("The Sum of this two numbers is : " + sum);
+
   }
 }
