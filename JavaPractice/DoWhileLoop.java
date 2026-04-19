be9572bfa2c6602682af46ec1d3fd913e6171094
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-04-19 21:31:37 +0530

    feat: add do while loop example program and demonstrates basic iteration using do while loop.

---
+++ JavaPractice/DoWhileLoop.java
@@ -0,0 +1,28 @@
+public class DoWhileLoop {
+  public static void main(String[] args) {
+    /*A do-while loop is a control statement where the code block executes at least once, before checking the condition.
+     A do-while loop executes the body first and then evaluates the condition. This guarantees that the loop runs at least one time, even if the condition is false.  
+    */
+
+     /* do while loop
+        syntax: 
+
+     1st:   initialization
+     
+     2nd:   execution
+            do{
+                 //body
+            }
+            while(condition);
+
+     */
+    
+
+      int a = 5;
+
+      do{
+        System.out.println("Hello Abhishek (>:| ");
+      } while(a != 5);
+  }
+  
+}
