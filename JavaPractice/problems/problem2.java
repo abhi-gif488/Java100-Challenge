708f146a8b69b74720e32522fcf14f184e2a9bd8
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-04-20 00:11:10 +0530

    docs: explain how we can trim characters using their index number

---
+++ JavaPractice/problems/problem2.java
@@ -0,0 +1,30 @@
+package problems;
+
+import java.util.Scanner;
+
+public class problem2 {
+  public static void main(String[] args) {
+    
+    System.out.print(" enter a character: ");
+
+    Scanner sc= new Scanner(System.in);
+
+    char ch = sc.next().trim().charAt(0);
+    
+    System.out.println(ch);
+    
+    if(ch>='a' && ch<='z'){
+      System.out.println("Lowercase");
+    }
+
+    else{
+      System.out.println("Uppercase");
+    }
+
+
+  }
+/* This Java program reads a single character from the user and determines whether it is lowercase or uppercase. It uses a `Scanner` to take input, trims any extra spaces, and extracts the first character entered. The program then checks if the character falls between `'a'` and `'z'` in ASCII values; if so, it prints “Lowercase”. Otherwise, it prints “Uppercase”. Finally, it also displays the entered character before showing the result.
+ */
+   
+  
+}
