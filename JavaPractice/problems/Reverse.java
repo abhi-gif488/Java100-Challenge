e29df0020604cd2bad264b490a2d3c5206c21c45
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-04-24 23:58:39 +0530

    docs: explain some of the syntax for better clearifications

--- JavaPractice/problems/Reverse.java
+++ JavaPractice/problems/Reverse.java
@@ -10,10 +10,10 @@ public class Reverse {
     System.out.println(" You have written " + num);
 
     int reverse= 0;
-    while (num>0) {
-      int rem= num%10;
-      reverse=reverse*10+rem;
-      num /=10;
+    while (num>0) {  //loop will run until num is greater than 0
+      int rem= num%10; // it will help to find the last digit as an remainder 
+      reverse=reverse*10+rem; //this will help to find the reverse of the given number at the last step
+      num /=10; //this line will remove the last digit of the number
 
     }
     System.out.println("So the reverse of number is : "+ reverse);
