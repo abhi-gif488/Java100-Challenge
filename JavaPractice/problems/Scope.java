09c8f4256985c88dcb22cefe5128eb8659eb24b5
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-07 19:52:46 +0530

    feat: add a program to execute scoping

---
+++ JavaPractice/problems/Scope.java
@@ -0,0 +1,26 @@
+package problems;
+
+public class Scope {
+  public static void main(String[] args){
+
+  int a = 20;
+  int b= 25;
+
+  System.out.println("a is equals to: "+a);
+  System.out.println("b is equals to: "+b);
+  
+
+{
+  //int a = 90; 
+   a = 50;
+  System.out.println("a is equals to: "+a);
+
+}
+
+  System.out.println("a is equals to: "+a);
+
+  }
+    
+  }
+  
+
