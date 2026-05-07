1674e4e709edb6f5a196d534723e32fe1652984c
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-07 21:57:58 +0530

    docs: explain aboutb Arrays.toString() method

--- JavaPractice/VarArgs.java
+++ JavaPractice/VarArgs.java
@@ -9,8 +9,11 @@ public class VarArgs {
   }
   static void fun(int ...v){  //this three dots '...' means taking input or value as n number of array value of any datatype
   //static void fun(String ...v) this way..
-  
+
     System.out.println(Arrays.toString(v));
-  }
+    /*It is a utility method used to convert a one-dimentional array into a readable string
+      instead of getting a confusing memory address(like [I@123a439b]), this method displays the actual contents of the array 'v' */
+  
+    }
   
 }
