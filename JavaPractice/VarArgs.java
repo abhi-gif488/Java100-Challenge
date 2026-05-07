fdb74d323a60620b3a1b6779e2ee8ad3dceb0777
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-07 21:53:13 +0530

    feat: add a program to explain variable length arguments

---
+++ JavaPractice/VarArgs.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class VarArgs {
+
+  //here we talk about variable length argument
+
+  public static void main(String[] args) {
+    fun(2,3,4,4,5,5,6,6,7,8);    //initilising the array value 
+  }
+  static void fun(int ...v){  //this three dots '...' means taking input or value as n number of array value of any datatype
+  //static void fun(String ...v) this way..
+  
+    System.out.println(Arrays.toString(v));
+  }
+  
+}
