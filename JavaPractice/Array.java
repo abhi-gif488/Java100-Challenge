55384ed6ce5146cb345a028965eb31eee630f447
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-08 23:49:18 +0530

    feat: add a basic program to understand array

---
+++ JavaPractice/Array.java
@@ -0,0 +1,13 @@
+public class Array {
+   public static void main(String[] args) {
+    System.out.println("Here we learn about array basics");
+    int[] arr = new int[5];
+    arr[0]=5;
+    arr[1]=7;
+     arr[2]=4;
+    arr[3]=9;
+     arr[4]=8;
+     System.out.println("The value of the given index is: "+ arr[3]);
+   }
+  
+}
