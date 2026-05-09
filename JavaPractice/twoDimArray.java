fc21bd7f162e59ef66deace857f67bf2a4c63239
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-09 22:49:09 +0530

    feat: add java program for 2D array input using for loops

---
+++ JavaPractice/twoDimArray.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class twoDimArray { 
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int[][] arr = new int[3][2];
+    System.out.println("length of the array is: "+arr.length);
+
+
+    //input
+
+for (int row = 0; row <3; row++) {
+
+  //now we take every row
+  for(int col=0; col<arr[row].length; col++){
+    arr[row][col]= in.nextInt();
+    
+  }
+  // first method to get output
+  
+}
+  for(int row=0; row<3; row++){
+    for(int col=0; col<arr[row].length; col++){
+        System.out.print(arr[row][col]+ " ");
+
+    }
+    System.out.println();
+   }
+
+  }
+
+  }
