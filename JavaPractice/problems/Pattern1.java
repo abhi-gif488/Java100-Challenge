e1e69e3c5baf3bc42398fda19a3f9b80f866b5ea
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-11 19:12:39 +0530

    feat: add program to show pattern using forloop

---
+++ JavaPractice/problems/Pattern1.java
@@ -0,0 +1,19 @@
+package problems;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Start small. Ship something.");
+        int star= sc.nextInt();
+        for(int i=star; i>=1; i--){
+            for(int j=1; j<=i; j++){
+            System.out.print("*");
+            
+            }
+            System.out.println();
+        }
+    }
+}
+  
