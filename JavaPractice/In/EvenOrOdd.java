fd391ddc1772b3a71caa2749b25c00b1b23fb724
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-13 23:52:18 +0530

    docs: explain program logic line by line

--- JavaPractice/In/EvenOrOdd.java
+++ JavaPractice/In/EvenOrOdd.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class EvenOrOdd {
   public static void main(String[] args) {
     System.out.println("Enter your Fav number: ");
-    Scanner sc = new Scanner(System.in);
-    int num= sc.nextInt();
-   check(num);
+
+    Scanner sc = new Scanner(System.in);//scanner object
+    int num= sc.nextInt();//user input
+
+   check(num);//calling method
    
 
 }
-    static void check(int num){
+    static void check(int num){//method to check whether given num is even or odd
       
-    if (num %2==0) {
+    if (num %2==0) {//condition to check
       System.out.println("Your given number "+ num +" is Even");
       
     }else{
