7de8323cdf438ebc6a5b773d50f2450f678d93e3
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-13 23:31:11 +0530

    feat: add program to calculate subarray sums

--- JavaPractice/SumSubAarray.java
+++ JavaPractice/SumSubAarray.java
@@ -3,10 +3,16 @@ public class SumSubAarray {
   public static void main(String[] args) {
     System.out.println("All about Sum of subarrays");
 int[] arr = {1,2,3,4,5};
+
+     // Outer loop selects the starting index of subarray
     for (int i = 0; i < arr.length; i++) {
-      int sum= 0;
+      
+      int sum= 0; // Variable to store sum of elements
+
+       // Inner loop starts from current index i and moves till the end of array
       for(int j=i; j<arr.length;j++){
-        sum+= arr[j];
+        
+        sum+= arr[j]; // Adding current element into sum
       }
       System.out.print(sum+ " ");
       
