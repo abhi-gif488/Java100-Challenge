121f1e623d1ef055ea1a23372485dc37f5eb9d4b
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-13 23:23:40 +0530

    docs: explain 2D array program line by line

--- JavaPractice/Matrix.java
+++ JavaPractice/Matrix.java
@@ -3,18 +3,23 @@ import java.util.Scanner;
 
 public class Matrix{ 
   public static void main(String[] args) {
+   // Creating Scanner object to take input from user
     Scanner in = new Scanner(System.in);
+
+    // Declaring a 2D array with 3 rows and 2 columns
     int[][] arr = new int[3][2];
   
-for (int row = 0; row <3; row++) {
+for (int row = 0; row <3; row++) { // Outer loop is used to access each row
   
-  for(int col=0; col<arr[row].length; col++){
+  for(int col=0; col<arr[row].length; col++){ // Inner loop is used to access each column of the current row
+
+      // Taking input from user and storing it in the array
     arr[row][col]= in.nextInt();
     
   }
-}
+}  //enhanced for lo
   for( int[] elemt : arr){
-    System.out.println(Arrays.toString(elemt));
+    System.out.println(Arrays.toString(elemt));  // Arrays.toString() converts array row into readable string format
   }
 }
 }
