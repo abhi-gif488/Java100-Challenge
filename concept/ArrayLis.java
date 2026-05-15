335c2cf04acfebfe741012efb6a0822e812c9506
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-15 20:22:12 +0530

    feat: add program to perform task like updating, removing, and find size of the ArrayList

--- concept/ArrayLis.java
+++ concept/ArrayLis.java
@@ -10,8 +10,21 @@ public class ArrayLis {
     list.add(33);
     list.add(409);
 
+    //accessing elements
     System.out.println(list); //print ArrayList
-    System.out.println(list.get(2));
+    System.out.println(list.get(2));//print the element whose index is '2'
+
+    //updating elements
+    list.set(1, 200);
+
+    //size of ArrayList
+    list.size();
+
+    //removing elements
+    list.remove(0);
+    //removing elements by objects
+    list.remove(Integer.valueOf(11));
+
   }
   
 }
