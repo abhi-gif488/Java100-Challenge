344553c8544447b2023caca06f8af6716ee7404c
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-15 21:06:31 +0530

    feat: use method contains() to check element is available or not

--- concept/ArrayLis.java
+++ concept/ArrayLis.java
@@ -10,20 +10,39 @@ public class ArrayLis {
     list.add(33);
     list.add(409);
 
+ // checking element exists
+    boolean exists = list.contains(409);
+    System.out.println("Is 409 present? - " + exists);
+
     //accessing elements
     System.out.println(list); //print ArrayList
     System.out.println(list.get(2));//print the element whose index is '2'
 
-    //updating elements
-    list.set(1, 200);
+    // //updating elements
+    // list.set(1, 200);
 
-    //size of ArrayList
-    list.size();
+    // //size of ArrayList
+    // list.size();
 
-    //removing elements
-    list.remove(0);
-    //removing elements by objects
-    list.remove(Integer.valueOf(11));
+    // //removing elements
+    // list.remove(0);
+    // //removing elements by objects
+    // list.remove(Integer.valueOf(11));
+
+
+    // But the problem is:
+// list.contains(409);
+// contains() returns a boolean value (true or false)
+// You are not storing or printing that value thats why its nothing returns anything
+
+/*instead: 
+
+    boolean exists = list.contains(409);
+    System.out.println("Is 409 present? - " + exists);
+
+    */
+    
+    
 
   }
   
