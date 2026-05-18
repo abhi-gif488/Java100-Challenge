a8f48e46411ad202e364f2041b1746923037535a
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-19 00:13:31 +0530

    docs: explain how multiple functions work together to reverse an array

--- JavaPractice/In/ReverseArray.java
+++ JavaPractice/In/ReverseArray.java
@@ -5,30 +5,28 @@ import java.util.Arrays;
 public class ReverseArray{
 public static void main(String[] args){
   System.out.println("Here we will learn about Reversing an Array using swapping");
-int[] arr = {1,2,3,4,5,6,7,8,9,10};
+int[] arr = {1,2,3,4,5,6,7,8,9,10};// declared and initialised array
 
+reverse(arr); // calling reverse first becoz if we call swap first then reverse function will work after swapping the first and last element of the array 
 
+System.out.println(Arrays.toString(arr));// use to show readable array
 
-reverse(arr);
-
-System.out.println(Arrays.toString(arr));
-
-swap(arr, 0, 9);
+swap(arr, 0, 9); //passing value
 System.out.println(Arrays.toString(arr));
 
 }
-static void swap(int[] arr,int index1,int index2){
+static void swap(int[] arr,int index1,int index2){ //function for swapping elements
   int temp= arr[index1];
   arr[index1]= arr[index2];
   arr[index2]= temp;
 
 }
 
-static void reverse(int[] arr){
+static void reverse(int[] arr){ //function for reversing elements
   int start=0;
   int end= arr.length-1;
   while(start<end){
-    swap(arr, start, end);
+    swap(arr, start, end);// calling swap function
     start++;
     end--;
   }
