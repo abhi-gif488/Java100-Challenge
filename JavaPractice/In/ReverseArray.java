c74b2d6d32371fac6ae3bf5ef2616ffc6579b790
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-19 00:07:06 +0530

    feat: implement array reversal using element swapping

---
+++ JavaPractice/In/ReverseArray.java
@@ -0,0 +1,40 @@
+package In;
+
+import java.util.Arrays;
+
+public class ReverseArray{
+public static void main(String[] args){
+  System.out.println("Here we will learn about Reversing an Array using swapping");
+int[] arr = {1,2,3,4,5,6,7,8,9,10};
+
+
+
+reverse(arr);
+
+System.out.println(Arrays.toString(arr));
+
+swap(arr, 0, 9);
+System.out.println(Arrays.toString(arr));
+
+}
+static void swap(int[] arr,int index1,int index2){
+  int temp= arr[index1];
+  arr[index1]= arr[index2];
+  arr[index2]= temp;
+
+}
+
+static void reverse(int[] arr){
+  int start=0;
+  int end= arr.length-1;
+  while(start<end){
+    swap(arr, start, end);
+    start++;
+    end--;
+  }
+
+}
+
+
+
+}
