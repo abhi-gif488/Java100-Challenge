643e3f130d37e9d2446d0df7c96e9f36d2d92bb1
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-18 19:39:01 +0530

    docs: explain the logic behind swapping array values

--- JavaPractice/In/swapArray.java
+++ JavaPractice/In/swapArray.java
@@ -6,16 +6,16 @@ public class swapArray {
 
   public static void main(String[] args) {
     System.out.println("Here we will swap two elements of the given array");
-    int[] arr = {1,2,3,4,5,6,7,8};
+    int[] arr = {1,2,3,4,5,6,7,8}; //declared and initialised array
 
-    Swapp(arr,0,7);
-    System.out.println(Arrays.toString(arr));
+    Swapp(arr,0,7); //calling function and given array 'arr' and its index no. to pass values
+    System.out.println(Arrays.toString(arr));//it will generate string value - easy to read
 
   }
-    static void Swapp(int[] arry,int index1, int index2){
+    static void Swapp(int[] arry,int index1, int index2){  //fuction to swap two numbers where array-'arr', and its two index values are taken
 
 
-        int temp= arry[index1];
+        int temp= arry[index1];// logic
         arry[index1]=arry[index2];
         arry[index2]= temp;
 
