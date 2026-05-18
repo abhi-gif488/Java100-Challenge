cf9333d0be14838b6e658ee6e371e27c81ce03e1
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-18 19:41:52 +0530

    docs: giving more clarity about passing the value

--- JavaPractice/In/swapArray.java
+++ JavaPractice/In/swapArray.java
@@ -9,10 +9,10 @@ public class swapArray {
     int[] arr = {1,2,3,4,5,6,7,8}; //declared and initialised array
 
     Swapp(arr,0,7); //calling function and given array 'arr' and its index no. to pass values
-    System.out.println(Arrays.toString(arr));//it will generate string value - easy to read
+    System.out.println(Arrays.toString(arr));//it will generate string value - easy to read: passing in function
 
   }
-    static void Swapp(int[] arry,int index1, int index2){  //fuction to swap two numbers where array-'arr', and its two index values are taken
+    static void Swapp(int[] arry,int index1, int index2){  //fuction to swap two numbers where array-'arr', and its two index values are taken-- directly we can say when function is called then this get the values of array, and indexes
 
 
         int temp= arry[index1];// logic
