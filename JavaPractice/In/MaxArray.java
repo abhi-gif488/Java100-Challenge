538c6b8b8453b2d84afea28723d46ef3b4b8116f
Abhishek Kumar <ABHISHEKKUMAR06204@GMAIL.COM>
2026-05-18 22:58:23 +0530

    docs: explain the logic of the program

--- JavaPractice/In/MaxArray.java
+++ JavaPractice/In/MaxArray.java
@@ -5,17 +5,17 @@ public class MaxArray {
   public static void main(String[] args){
 
     System.out.println("Here we will find the max number in the Array");
-    int[] arr = {3,5,689,807,99,98};
-    int max= Maxx(arr);
+    int[] arr = {3,5,689,807,99,98}; //declared and initialised array
+    int max= Maxx(arr);  // declared max and calling function Maxx
     System.out.println(max);
 
   }
-  static int  Maxx(int[] arr){
+  static int  Maxx(int[] arr){  //function to find max no. of an array
     
 
-      int max=arr[0];
-      for (int i = 1; i < arr.length; i++) {
-        if (arr[i]>max) {
+      int max=arr[0]; //let assume 0th index number is maxzz
+      for (int i = 1; i < arr.length; i++) {  //use for loop so that iteration will move every elemet of the array
+        if (arr[i]>max) {   //give condition if ith element of array is greater than max then:  
           max= arr[i];
           
         }
